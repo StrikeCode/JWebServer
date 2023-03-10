de6620f1f3b882f03549444e8b87f7fa546e91c9
StrikeCode <CodeTracker@outlook.com>
2023-03-09 23:16:13 -0800

    feat:同步机制封装,http处理头文件声明

---
+++ lock/locker.h
@@ -0,0 +1,122 @@
+/*
+    2023年03月09日 20:16:35
+    将三种同步机制封装，将锁的创建与销毁函数封装在类的构造和析构中，实现RAII
+*/
+#ifndef LOCKER_H
+#define LOCKER_H
+
+#include <exception>
+#include <pthread.h>
+#include <semaphore.h>
+
+class sem
+{
+public:
+    sem()
+    {
+        // 第二个参数为0表示这个信号量是当前进程的局部信号量
+        // 否则该信号量就可以在多个进程之间共享
+        if (sem_init(&m_sem, 0, 0) != 0)
+        {
+            throw std::exception();
+        }
+    }
+    sem(int num)
+    {
+        if (sem_init(&m_sem, 0, num) != 0)
+        {
+            throw std::exception();
+        }
+    }
+    ~sem()
+    {
+        sem_destroy(&m_sem);
+    }
+    bool wait()
+    {
+        return sem_wait(&m_sem) == 0;
+    }
+    bool post()
+    {
+        return sem_post(&m_sem) == 0;
+    }
+
+private:
+    sem_t m_sem;
+};
+class locker
+{
+public:
+    locker()
+    {
+        if (pthread_mutex_init(&m_mutex, NULL) != 0)
+        {
+            throw std::exception();
+        }
+    }
+    ~locker()
+    {
+        pthread_mutex_destroy(&m_mutex);
+    }
+    bool lock()
+    {
+        return pthread_mutex_lock(&m_mutex) == 0;
+    }
+    bool unlock()
+    {
+        return pthread_mutex_unlock(&m_mutex) == 0;
+    }
+    pthread_mutex_t *get()
+    {
+        return &m_mutex;
+    }
+
+private:
+    pthread_mutex_t m_mutex;
+};
+class cond
+{
+public:
+    cond()
+    {
+        if (pthread_cond_init(&m_cond, NULL) != 0)
+        {
+            //pthread_mutex_destroy(&m_mutex);
+            throw std::exception();
+        }
+    }
+    ~cond()
+    {
+        pthread_cond_destroy(&m_cond);
+    }
+    bool wait(pthread_mutex_t *m_mutex)
+    {
+        int ret = 0;
+        //pthread_mutex_lock(&m_mutex);
+        // 内部有一次unlock和lock的操作
+        ret = pthread_cond_wait(&m_cond, m_mutex);
+        //pthread_mutex_unlock(&m_mutex);
+        return ret == 0;
+    }
+    bool timewait(pthread_mutex_t *m_mutex, struct timespec t)
+    {
+        int ret = 0;
+        //pthread_mutex_lock(&m_mutex);
+        ret = pthread_cond_timedwait(&m_cond, m_mutex, &t);
+        //pthread_mutex_unlock(&m_mutex);
+        return ret == 0;
+    }
+    bool signal()
+    {
+        return pthread_cond_signal(&m_cond) == 0;
+    }
+    bool broadcast()
+    {
+        return pthread_cond_broadcast(&m_cond) == 0;
+    }
+
+private:
+    //static pthread_mutex_t m_mutex;
+    pthread_cond_t m_cond;
+};
+#endif
